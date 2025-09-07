fd01b022b2e3d90f9c1006b03994dd0db4e3f08c
celsowm <celsowm@gmail.com>
2025-09-07 17:42:26 -0300

    no borders on list

--- tests/06_list_numbered.php
+++ tests/06_list_numbered.php
@@ -6,16 +6,10 @@ use Celsowm\PagyraPhp\Core\PdfBuilder;
 
 $pdf = new PdfBuilder();
 
-// lista simples numerada (prefixando manualmente os itens)
 $pdf->addList([
     'Primeiro passo',
     'Segundo passo',
     'Terceiro passo',
-], [
-    'spacing' => 6,
-    'align'   => 'left',
-    'padding' => 6,
-    'border'  => ['style' => 'solid'],
 ]);
 
 $pdf->save('06_list_numbered.pdf');
