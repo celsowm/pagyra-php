6d30827efdb394d4b93373688d016d2e6189b3e9
celsowm <celsowm@gmail.com>
2025-09-20 03:00:39 -0300

    new example

---
+++ tests/07_list_nested.php
@@ -0,0 +1,57 @@
+﻿<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Celsowm\PagyraPhp\Core\PdfBuilder;
+
+$pdf = new PdfBuilder();
+
+$pdf->addList([
+    [
+        'text' => 'Planejamento semanal',
+        'children' => [
+            [
+                'text' => 'Definir metas principais',
+                'children' => [
+                    'Listar prioridades',
+                    'Separar responsabilidades',
+                ],
+            ],
+            [
+                'text' => 'Organizar recursos',
+                'children' => [
+                    'Conferir ferramentas',
+                    'Alinhar agenda da equipe',
+                ],
+            ],
+        ],
+    ],
+    [
+        'text' => 'Execucao',
+        'children' => [
+            'Monitorar progresso',
+            'Registrar aprendizados',
+        ],
+    ],
+    [
+        'text' => 'Encerramento',
+        'children' => [
+            [
+                'text' => 'Revisao geral',
+                'children' => [
+                    'Compartilhar resultados',
+                    'Planejar proximos passos',
+                ],
+            ],
+        ],
+    ],
+], [
+    'typeByLevel' => ['upper-roman', 'decimal', 'bullet'],
+    'bulletCharsByLevel' => ['-', '>', '*'],
+    'levelIndent' => 22,
+    'gap' => 8,
+    'itemSpacing' => 4,
+    'markerSizeScale' => 0.9,
+]);
+
+$pdf->save('07_list_nested.pdf');
