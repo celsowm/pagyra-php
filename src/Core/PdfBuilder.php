785e1c01d4c211f52cb928c7c5fbedeeffad213d
celsowm <celsowm@gmail.com>
2025-09-22 23:57:34 -0300

    clean of old methods

--- src/Core/PdfBuilder.php
+++ src/Core/PdfBuilder.php
@@ -1837,65 +1837,9 @@ final class PdfBuilder
         file_put_contents($path, $this->output());
     }
 
-    private function emitFontObjects(): array
-    {
-        $type0Ids = [];
-        foreach ($this->fonts as $alias => $font) {
-            if (empty($this->usedGids[$alias])) continue;
-
-            $spaceGid = $font['cmap'][0x20] ?? null;
-            $dwUnits = $spaceGid !== null ? (int)$font['adv'][$spaceGid] : (int)round(array_sum($font['adv']) / max(1, count($font['adv'])));
-            $DW = (int)round(($dwUnits * 1000) / $font['unitsPerEm']);
-            $W = [];
-            foreach ($this->usedGids[$alias] as $gid => $_) {
-                $w = (int)round((($font['adv'][$gid] ?? $dwUnits) * 1000) / $font['unitsPerEm']);
-                if ($w !== $DW) $W[] = "{$gid} [{$w}]";
-            }
-            $WArr = count($W) ? "/W [ " . implode(' ', $W) . " ]" : "";
-            $toUniId = $this->newObjectId();
-            $this->setObject($toUniId, PdfStreamBuilder::streamObj($this->buildToUnicodeCMap($font['name'], $this->usedGids[$alias])));
-            $fileId = $this->newObjectId();
-            $this->setObject($fileId, PdfStreamBuilder::streamObj($font['ttf']));
-            $descId = $this->newObjectId();
-            $bboxPdf = array_map(fn($v) => (int)round(($v * 1000) / $font['unitsPerEm']), $font['bbox']);
-            $ascent = (int)round(($font['ascent'] * 1000) / $font['unitsPerEm']);
-            $descent = (int)round(($font['descent'] * 1000) / $font['unitsPerEm']);
-            $italicAnglePdf = (int)round(($font['italicAngle'] ?? 0.0));
-            $this->setObject($descId, "<< /Type /FontDescriptor /FontName /{$font['name']} /Flags 32 " .
-                "/Ascent {$ascent} /Descent {$descent} /CapHeight {$ascent} /ItalicAngle {$italicAnglePdf} " .
-                "/FontBBox [ {$bboxPdf[0]} {$bboxPdf[1]} {$bboxPdf[2]} {$bboxPdf[3]} ] " .
-                "/StemV 80 /FontFile2 {$fileId} 0 R >>");
-            $cidId = $this->newObjectId();
-            $cidInfo = "/CIDSystemInfo << /Registry (Adobe) /Ordering (Identity) /Supplement 0 >>";
-            $this->setObject($cidId, "<< /Type /Font /Subtype /CIDFontType2 /BaseFont /{$font['name']} {$cidInfo} /CIDToGIDMap /Identity /DW {$DW} {$WArr} /FontDescriptor {$descId} 0 R >>");
-            $type0Id = $this->newObjectId();
-            $this->setObject($type0Id, "<< /Type /Font /Subtype /Type0 /BaseFont /{$font['name']} /Encoding /Identity-H /DescendantFonts [ {$cidId} 0 R ] /ToUnicode {$toUniId} 0 R >>");
-            $type0Ids[$alias] = $type0Id;
-        }
-        return $type0Ids;
-    }
-
-    private function buildToUnicodeCMap(string $psName, array $gidToUni): string
-    {
-        $pairs = [];
-        foreach ($gidToUni as $gid => $uni) $pairs[] = sprintf("<%04X> <%04X>\n", $gid, $uni);
-        $chunks = array_chunk($pairs, 100);
-        $bf = '';
-        foreach ($chunks as $chunk) $bf .= count($chunk) . " beginbfchar\n" . implode('', $chunk) . "endbfchar\n";
-        return "/CIDInit /ProcSet findresource begin\n12 dict begin\nbegincmap\n" .
-            "/CIDSystemInfo << /Registry (Adobe) /Ordering (Identity) /Supplement 0 >> def\n" .
-            "/CMapName /{$psName} def\n/CMapType 2 def\n1 begincodespacerange\n<0000> <FFFF>\nendcodespacerange\n" .
-            $bf . "endcmap\nCMapName currentdict /CMap defineresource pop\nend\nend\n";
-    }
-
     public function newObjectId(): int
     {
         return $this->writer->newObjectId();
-    }
-
-    private function sanitizePSName(string $s): string
-    {
-        return preg_replace('/[^A-Za-z0-9\-\+_]/', '', str_replace(' ', '-', $s)) ?: 'Embedded';
     }
 
     private function normalizeShadowSpec($spec): ?array
