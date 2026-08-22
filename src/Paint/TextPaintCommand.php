f02dd42203e1e16904392c3de36db9bc66c83eab
celsowm <celsowm@gmail.com>
2026-08-22 16:04:47 -0300

    add text paint command model

---
+++ src/Paint/TextPaintCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pagyra\Paint;
+
+use Pagyra\Css\Color\Rgba;
+use Pagyra\Layout\TextRun;
+
+final readonly class TextPaintCommand implements \JsonSerializable
+{
+    public function __construct(
+        public TextRun $run,
+        public int $pageIndex,
+        public float $x,
+        public float $y,
+        public float $baseline,
+        public string $text,
+        public float $fontSize,
+        public ?string $fontFamily,
+        public int $fontWeight,
+        public string $fontStyle,
+        public ?Rgba $color,
+    ) {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'text',
+            'pageIndex' => $this->pageIndex,
+            'x' => $this->x,
+            'y' => $this->y,
+            'baseline' => $this->baseline,
+            'text' => $this->text,
+            'fontSize' => $this->fontSize,
+            'fontFamily' => $this->fontFamily,
+            'fontWeight' => $this->fontWeight,
+            'fontStyle' => $this->fontStyle,
+            'color' => $this->color,
+            'run' => $this->run,
+        ];
+    }
+}
