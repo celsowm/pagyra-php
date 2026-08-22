812b90b29c246c91c30a2653b24f2d653ea3eca2
celsowm <celsowm@gmail.com>
2026-08-22 11:03:13 -0300

    test first text line layout

---
+++ tests/Integration/TextLayoutTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pagyra\Tests\Integration;
+
+use Pagyra\Pagyra;
+use PHPUnit\Framework\TestCase;
+
+final class TextLayoutTest extends TestCase
+{
+    public function testParagraphGetsLineBoxAndIntrinsicHeight(): void
+    {
+        $prepared = Pagyra::prepareHtmlRender([
+            'html' => '<p style="margin:0;font-size:20px;line-height:1.5;width:200px">Hello world</p>',
+            'viewportWidth' => 400,
+            'viewportHeight' => 600,
+        ]);
+
+        $paragraph = $prepared->layoutRoot->children[0];
+        self::assertCount(1, $paragraph->lineBoxes);
+        self::assertSame('Hello world', $paragraph->lineBoxes[0]->text);
+        self::assertSame(30.0, $paragraph->lineBoxes[0]->height);
+        self::assertSame(30.0, $paragraph->box->content->height);
+    }
+
+    public function testNarrowParagraphWrapsAndGrowsVertically(): void
+    {
+        $prepared = Pagyra::prepareHtmlRender([
+            'html' => '<p style="margin:0;font-size:16px;line-height:1.5;width:70px">hello hello hello</p>',
+            'viewportWidth' => 400,
+            'viewportHeight' => 600,
+        ]);
+
+        $paragraph = $prepared->layoutRoot->children[0];
+        self::assertCount(3, $paragraph->lineBoxes);
+        self::assertSame(72.0, $paragraph->box->content->height);
+        self::assertSame(0.0, $paragraph->lineBoxes[0]->y);
+        self::assertSame(24.0, $paragraph->lineBoxes[1]->y);
+        self::assertSame(48.0, $paragraph->lineBoxes[2]->y);
+    }
+
+    public function testHeadingAndParagraphOccupyRealVerticalSpace(): void
+    {
+        $prepared = Pagyra::prepareHtmlRender([
+            'html' => '<h1>Título</h1><p>Todo poder emana do povo.</p>',
+            'viewportWidth' => 500,
+            'viewportHeight' => 700,
+        ]);
+
+        $heading = $prepared->layoutRoot->children[0];
+        $paragraph = $prepared->layoutRoot->children[1];
+
+        self::assertNotEmpty($heading->lineBoxes);
+        self::assertNotEmpty($paragraph->lineBoxes);
+        self::assertGreaterThan(0.0, $heading->box->content->height);
+        self::assertGreaterThan(0.0, $paragraph->box->content->height);
+        self::assertGreaterThan($heading->box->borderBox()->bottom(), $paragraph->box->borderBox()->y);
+    }
+}
