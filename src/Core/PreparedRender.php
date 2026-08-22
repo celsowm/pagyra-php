0a94ce791ae4ea5e900538329f00ceb63fcb551b
celsowm <celsowm@gmail.com>
2026-08-22 10:00:42 -0300

    expose styled tree in prepared render

--- src/Core/PreparedRender.php
+++ src/Core/PreparedRender.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace Pagyra\Core;
 
 use Pagyra\Dom\Node;
+use Pagyra\Style\StyledNode;
 
 final readonly class PreparedRender implements \JsonSerializable
 {
-    /** @param array{widthPt:float,heightPt:float} $pageSize
-     *  @param array{top:float,right:float,bottom:float,left:float} $margins
+    /**
+     * @param array{widthPt:float,heightPt:float} $pageSize
+     * @param array{top:float,right:float,bottom:float,left:float} $margins
+     * @param list<string> $stylesheetHrefs
      */
     public function __construct(
         public Node $domRoot,
+        public StyledNode $styledRoot,
+        public string $cssText,
+        public array $stylesheetHrefs,
         public array $pageSize,
         public array $margins,
     ) {
@@ -22,6 +28,9 @@ final readonly class PreparedRender implements \JsonSerializable
     {
         return [
             'domRoot' => $this->domRoot,
+            'styledRoot' => $this->styledRoot,
+            'cssText' => $this->cssText,
+            'stylesheetHrefs' => $this->stylesheetHrefs,
             'pageSize' => $this->pageSize,
             'margins' => $this->margins,
         ];
