fcae409d473af1d17e0e5c1ea92a2c3b1ebb3e73
celsowm <celsowm@gmail.com>
2026-08-22 18:01:33 -0300

    test composite glyph subset closure

--- tests/Unit/Fonts/TtfSubsetterTest.php
+++ tests/Unit/Fonts/TtfSubsetterTest.php
@@ -27,12 +27,49 @@ final class TtfSubsetterTest extends TestCase
         self::assertSame(610, $metrics->advanceWidth(2));
     }
 
+    public function testCompositeGlyphKeepsReferencedComponent(): void
+    {
+        $font = $this->compositeFixture();
+        $subset = (new TtfSubsetter())->subset($font, [2]);
+
+        self::assertNotNull($subset);
+        $tables = $this->directory($subset);
+        $loca = substr($subset, $tables['loca']['offset'], $tables['loca']['length']);
+        $offset1 = unpack('N', substr($loca, 4, 4))[1];
+        $offset2 = unpack('N', substr($loca, 8, 4))[1];
+        self::assertGreaterThan($offset1, $offset2, 'gid 1 component must remain present in sparse subset');
+    }
+
     public function testFallsBackWithNullWhenRequiredTablesAreMissing(): void
     {
         self::assertNull((new TtfSubsetter())->subset("\x00\x01\x00\x00" . str_repeat("\0", 20), [1]));
     }
 
     private function fontFixture(): string
+    {
+        return $this->buildFixture([
+            pack('n', 0) . str_repeat("\0", 8),
+            pack('n', 0) . str_repeat("\x11", 8),
+            pack('n', 0) . str_repeat("\x22", 8),
+            pack('n', 0) . str_repeat("\x33", 8),
+        ]);
+    }
+
+    private function compositeFixture(): string
+    {
+        $component = pack('n', 0) . str_repeat("\x11", 8);
+        $composite = pack('n', 0xFFFF) . str_repeat("\0", 8)
+            . pack('nncc', 0, 1, 0, 0);
+        return $this->buildFixture([
+            pack('n', 0) . str_repeat("\0", 8),
+            $component,
+            $composite,
+            pack('n', 0) . str_repeat("\x33", 8),
+        ]);
+    }
+
+    /** @param list<string> $glyphs */
+    private function buildFixture(array $glyphs): string
     {
         $head = str_repeat("\0", 54);
         $head = substr_replace($head, pack('n', 1000), 18, 2);
@@ -41,25 +78,24 @@ final class TtfSubsetterTest extends TestCase
         $hhea = str_repeat("\0", 36);
         $hhea = substr_replace($hhea, pack('n', 800), 4, 2);
         $hhea = substr_replace($hhea, pack('n', 0x10000 - 200), 6, 2);
-        $hhea = substr_replace($hhea, pack('n', 4), 34, 2);
+        $hhea = substr_replace($hhea, pack('n', count($glyphs)), 34, 2);
 
         $maxp = str_repeat("\0", 32);
-        $maxp = substr_replace($maxp, pack('n', 4), 4, 2);
-        $hmtx = pack('nnnnnnnn', 500, 0, 600, 0, 610, 0, 620, 0);
+        $maxp = substr_replace($maxp, pack('n', count($glyphs)), 4, 2);
+        $hmtx = '';
+        foreach ($glyphs as $gid => $_) $hmtx .= pack('nn', 500 + $gid * 10, 0);
 
-        $glyph0 = pack('n', 0) . str_repeat("\0", 8);
-        $glyph1 = pack('n', 0) . str_repeat("\x11", 8);
-        $glyph2 = pack('n', 0) . str_repeat("\x22", 8);
-        $glyph3 = pack('n', 0) . str_repeat("\x33", 8);
-        $glyf = $glyph0 . $glyph1 . $glyph2 . $glyph3;
-        $loca = pack('NNNNN', 0, 10, 20, 30, 40);
+        $glyf = '';
+        $offsets = [0];
+        foreach ($glyphs as $glyph) {
+            $glyf .= $glyph;
+            $offsets[] = strlen($glyf);
+        }
+        $loca = '';
+        foreach ($offsets as $offset) $loca .= pack('N', $offset);
 
         $cmap12 = pack('nnNNN', 12, 0, 28, 0, 1) . pack('NNN', 65, 68, 0);
         $cmap = pack('nnnnN', 0, 1, 3, 10, 12) . $cmap12;
-
-        $name = pack('nnn', 0, 0, 6);
-        $post = pack('N', 0x00030000) . str_repeat("\0", 28);
-
         $tables = [
             'head' => $head,
             'hhea' => $hhea,
@@ -68,8 +104,8 @@ final class TtfSubsetterTest extends TestCase
             'loca' => $loca,
             'glyf' => $glyf,
             'cmap' => $cmap,
-            'name' => $name,
-            'post' => $post,
+            'name' => pack('nnn', 0, 0, 6),
+            'post' => pack('N', 0x00030000) . str_repeat("\0", 28),
         ];
         ksort($tables, SORT_STRING);
 
@@ -84,7 +120,22 @@ final class TtfSubsetterTest extends TestCase
             if ($padding) $payload .= str_repeat("\0", $padding);
             $offset += strlen($data) + $padding;
         }
-
         return pack('Nnnnn', 0x00010000, $count, 0, 0, 0) . $directory . $payload;
+    }
+
+    /** @return array<string,array{offset:int,length:int}> */
+    private function directory(string $font): array
+    {
+        $count = unpack('n', substr($font, 4, 2))[1];
+        $tables = [];
+        for ($i = 0; $i < $count; $i++) {
+            $p = 12 + $i * 16;
+            $tag = substr($font, $p, 4);
+            $tables[$tag] = [
+                'offset' => unpack('N', substr($font, $p + 8, 4))[1],
+                'length' => unpack('N', substr($font, $p + 12, 4))[1],
+            ];
+        }
+        return $tables;
     }
 }
