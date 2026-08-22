fb8fde1b525147b8c94ac0076db3dad2cf22a452
celsowm <celsowm@gmail.com>
2026-08-22 15:48:01 -0300

    add page placement value object

---
+++ src/Pagination/PagePlacement.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pagyra\Pagination;
+
+use Pagyra\Layout\LayoutNode;
+
+final readonly class PagePlacement implements \JsonSerializable
+{
+    public function __construct(
+        public LayoutNode $node,
+        public int $pageIndex,
+        public float $offsetY,
+        public float $startY,
+        public float $endY,
+    ) {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'pageIndex' => $this->pageIndex,
+            'offsetY' => $this->offsetY,
+            'startY' => $this->startY,
+            'endY' => $this->endY,
+            'node' => $this->node,
+        ];
+    }
+}
