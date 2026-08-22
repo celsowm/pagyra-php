2479fbb92619ad7d13ed80583a74c9a6af5ff7f1
celsowm <celsowm@gmail.com>
2026-08-22 18:35:39 -0300

    model png image data for pdf embedding

---
+++ src/Pdf/PngPdfImageData.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pagyra\Pdf;
+
+final readonly class PngPdfImageData
+{
+    public function __construct(
+        public int $width,
+        public int $height,
+        public int $bitsPerComponent,
+        public int $colors,
+        public string $colorSpace,
+        public string $compressedData,
+    ) {
+    }
+}
