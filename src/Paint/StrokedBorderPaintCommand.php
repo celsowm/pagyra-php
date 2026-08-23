4ee5840fd773f103fac5f51ff18e8990f918fe85
celsowm <celsowm@gmail.com>
2026-08-22 23:21:04 -0300

    specialize stroked borders from border command

--- src/Paint/StrokedBorderPaintCommand.php
+++ src/Paint/StrokedBorderPaintCommand.php
@@ -8,29 +8,36 @@ use Pagyra\Css\Color\Rgba;
 use Pagyra\Layout\AtomicInlineBox;
 use Pagyra\Layout\LayoutNode;
 
-final readonly class StrokedBorderPaintCommand implements \JsonSerializable
+final readonly class StrokedBorderPaintCommand extends BorderPaintCommand
 {
     /** @param list<float> $dashPattern */
     public function __construct(
-        public LayoutNode|AtomicInlineBox $node,
-        public int $pageIndex,
-        public string $side,
+        LayoutNode|AtomicInlineBox $node,
+        int $pageIndex,
+        string $side,
         public float $x1,
         public float $y1,
         public float $x2,
         public float $y2,
         public float $lineWidth,
-        public Rgba $color,
+        Rgba $color,
         public array $dashPattern,
         public float $dashPhase = 0.0,
         public string $lineCap = 'butt',
     ) {
-        if (!in_array($side, ['top', 'right', 'bottom', 'left'], true)) {
-            throw new \InvalidArgumentException('Unsupported border side');
-        }
         if (!in_array($lineCap, ['butt', 'round', 'square'], true)) {
             throw new \InvalidArgumentException('Unsupported line cap');
         }
+        parent::__construct(
+            node: $node,
+            pageIndex: $pageIndex,
+            side: $side,
+            x: min($x1, $x2),
+            y: min($y1, $y2),
+            width: abs($x2 - $x1),
+            height: abs($y2 - $y1),
+            color: $color,
+        );
     }
 
     public function jsonSerialize(): array
