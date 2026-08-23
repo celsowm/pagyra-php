1d4cb0dc78c0e601068381eaaaca940ec570ad39
celsowm <celsowm@gmail.com>
2026-08-23 19:53:46 -0300

    test svg elliptical arc normalization

--- tests/Unit/Svg/PathDataParserTest.php
+++ tests/Unit/Svg/PathDataParserTest.php
@@ -65,4 +65,46 @@ final class PathDataParserTest extends TestCase
         self::assertSame(['type' => 'M', 'x' => 10.0, 'y' => -2.0], $segments[0]);
         self::assertSame(['type' => 'L', 'x' => 0.5, 'y' => 3.0], $segments[1]);
     }
+
+    public function testConvertsHalfCircleArcToTwoCubicSegments(): void
+    {
+        $segments = (new PathDataParser())->parse('M0 0 A 10 10 0 0 1 20 0');
+
+        self::assertCount(3, $segments);
+        self::assertSame('C', $segments[1]['type']);
+        self::assertSame('C', $segments[2]['type']);
+        self::assertEqualsWithDelta(20.0, $segments[2]['x'], 0.000001);
+        self::assertEqualsWithDelta(0.0, $segments[2]['y'], 0.000001);
+        self::assertEqualsWithDelta(10.0, $segments[1]['x'], 0.000001);
+        self::assertEqualsWithDelta(-10.0, $segments[1]['y'], 0.000001);
+    }
+
+    public function testSupportsRelativeArcAndCorrectsTooSmallRadii(): void
+    {
+        $segments = (new PathDataParser())->parse('M10 10 a 2 2 0 0 1 10 0');
+
+        self::assertGreaterThanOrEqual(2, count($segments));
+        $last = $segments[array_key_last($segments)];
+        self::assertSame('C', $last['type']);
+        self::assertEqualsWithDelta(20.0, $last['x'], 0.000001);
+        self::assertEqualsWithDelta(10.0, $last['y'], 0.000001);
+    }
+
+    public function testDegenerateArcWithZeroRadiusBecomesLine(): void
+    {
+        $segments = (new PathDataParser())->parse('M1 2 A 0 10 0 0 1 7 8');
+
+        self::assertSame(['type' => 'L', 'x' => 7.0, 'y' => 8.0], $segments[1]);
+    }
+
+    public function testArcAcceptsNumericFlagsLikeReferenceParser(): void
+    {
+        $segments = (new PathDataParser())->parse('M0 0 A10 10 0 2 -3 20 0');
+
+        self::assertNotEmpty($segments);
+        $last = $segments[array_key_last($segments)];
+        self::assertSame('C', $last['type']);
+        self::assertEqualsWithDelta(20.0, $last['x'], 0.000001);
+        self::assertEqualsWithDelta(0.0, $last['y'], 0.000001);
+    }
 }
