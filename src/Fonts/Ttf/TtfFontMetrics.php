4de1bd5de43ef0ecc5dd2e198900f7f439098516
celsowm <celsowm@gmail.com>
2026-09-25 13:28:07 -0300

    Represent class-based GPOS kerning rules

--- src/Fonts/Ttf/TtfFontMetrics.php
+++ src/Fonts/Ttf/TtfFontMetrics.php
@@ -11,6 +11,7 @@ final readonly class TtfFontMetrics
      * @param array<int,int> $cmap
      * @param array<int,array<int,int>> $kerning
      * @param array{xMin:int,yMin:int,xMax:int,yMax:int} $bbox
+     * @param list<array{coverage:array<int,bool>,class1:array<int,int>,class2:array<int,int>,values:array<int,array<int,int>>}> $classKerning
      */
     public function __construct(
         public int $unitsPerEm,
@@ -21,6 +22,7 @@ final readonly class TtfFontMetrics
         public array $cmap,
         public array $kerning = [],
         public array $bbox = ['xMin' => 0, 'yMin' => 0, 'xMax' => 0, 'yMax' => 0],
+        public array $classKerning = [],
     ) {
         if ($unitsPerEm <= 0) {
             throw new \InvalidArgumentException('unitsPerEm must be greater than zero');
@@ -39,6 +41,13 @@ final readonly class TtfFontMetrics
 
     public function kerning(int $leftGlyph, int $rightGlyph): int
     {
-        return $this->kerning[$leftGlyph][$rightGlyph] ?? 0;
+        $value = $this->kerning[$leftGlyph][$rightGlyph] ?? 0;
+        foreach ($this->classKerning as $rule) {
+            if (!isset($rule['coverage'][$leftGlyph])) continue;
+            $leftClass = $rule['class1'][$leftGlyph] ?? 0;
+            $rightClass = $rule['class2'][$rightGlyph] ?? 0;
+            $value += $rule['values'][$leftClass][$rightClass] ?? 0;
+        }
+        return $value;
     }
 }
